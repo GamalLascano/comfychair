f1015553a8838517f8fc417e69ad079eb2376a56
GamalLascano <gamallascano@gmail.com>
2024-09-13 19:06:32 -0300

    Refactored test

--- track/track.test.ts
+++ track/track.test.ts
@@ -2,6 +2,13 @@ import { expect, jest, test } from "@jest/globals";
 import Usuario from "../user/usuario";
 import Conferencia from "../conference/conferencia";
 test("Crear conferencia", () => {
+  const conferencia = crearConferencia();
+  expect(conferencia.getAutores().length).toBe(4);
+  expect(conferencia.getChairs().length).toBe(2);
+  expect(conferencia.getRevisores().length).toBe(3);
+});
+test("Crear Track", () => {});
+function crearConferencia() {
   const pablo = new Usuario("Pablo", "UNLP", "pablo@unlp.edu.ar", "1234");
   const pedro = new Usuario("Pedro", "UNLP", "pedro@unlp.edu.ar", "1234");
   const patricio = new Usuario(
@@ -17,7 +24,5 @@ test("Crear conferencia", () => {
   const revisores = [pablo, pedro, tomas];
   const autores = [patricio, pablo, pedro, maria];
   const conferencia = new Conferencia(chairs, revisores, autores);
-  expect(conferencia.getAutores().length).toBe(4);
-  expect(conferencia.getChairs().length).toBe(2);
-  expect(conferencia.getRevisores().length).toBe(3);
-});
+  return conferencia;
+}
